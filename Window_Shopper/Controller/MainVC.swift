160908ec40a7894287e5786474041fc49a63121c
Francisco Salom <FranSalom@MBPdeFrancisco.cantv.net>
2017-10-03 12:41:34 -0400

    Creando Window Shopper app

---
+++ Window_Shopper/Controller/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Window_Shopper
+//
+//  Created by Francisco Salom on 3/10/17.
+//  Copyright © 2017 Francisco Salom. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+    }
+
+}
+
---
+++ Window_Shopper/View/TextFieldView.swift
@@ -0,0 +1,15 @@
+//
+//  TextFieldView.swift
+//  Window_Shopper
+//
+//  Created by Francisco Salom on 3/10/17.
+//  Copyright © 2017 Francisco Salom. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class TextFieldView: UITextField {
+
+    
+
+}

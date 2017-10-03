10ad6416bcd150d94aa90790745cb45cd6b1be96
Francisco Salom <FranSalom@MBPdeFrancisco.cantv.net>
2017-10-03 16:21:34 -0400

    Creando boton calcular

--- Window_Shopper/Controller/MainVC.swift
+++ Window_Shopper/Controller/MainVC.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var hourtxt: TextFieldView!
+    @IBOutlet weak var itemtxt: TextFieldView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            
+    
+    let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        
+        calcBtn.backgroundColor = #colorLiteral(red: 0.84812361, green: 0.2746934295, blue: 0.1194182709, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculateHours), for: .touchUpInside)
+        
+        hourtxt.inputAccessoryView = calcBtn
+        itemtxt.inputAccessoryView = calcBtn
+    }
+    
+    @objc func calculateHours(){
+        var hour: String!
+    
+        hour = hourtxt.text
+        print(hour)
+        
     }
 
 }
